3d15a16e723b2c241180645613cb1184154dc9ba
zhang wenqiang <zhangwenqiang@kingsoft.com>
2024-07-16 18:27:21 +0800

    feat:增加默认值检测;增加跳过注解逻辑

--- src/main/java/com/ksyun/train/util/ParamParseUtil.java
+++ src/main/java/com/ksyun/train/util/ParamParseUtil.java
@@ -10,9 +10,9 @@ import java.util.List;
 import java.util.Stack;
 
 public class ParamParseUtil {
-    private final static Stack<Object>      objStack = new Stack<>();            //存放对象栈
-    private final static Stack<Integer>     preSpaceStack = new Stack<>();       //存放缩进栈
-    private final static Stack<Class<?>>    innerClazzStack = new Stack<>();     //存放泛型参数类型栈     //存放泛型参数类型
+    private final static Stack<Object> objStack = new Stack<>();            //存放对象栈
+    private final static Stack<Integer> preSpaceStack = new Stack<>();       //存放缩进栈
+    private final static Stack<Class<?>> innerClazzStack = new Stack<>();     //存放泛型参数类型栈     //存放泛型参数类型
 
     public static int getPreSpaceNum(String s) {
         int preSpaceNum = 0;
@@ -29,7 +29,7 @@ public class ParamParseUtil {
     public static int getFirstIndexIgnoreFirstChar(String s, char c) {
         //find first index of letter
 
-        for (int i = s.indexOf(c)+1; i < s.length(); i++) {
+        for (int i = s.indexOf(c) + 1; i < s.length(); i++) {
             if (s.charAt(i) != ' ') {
                 return i;
             }
@@ -37,22 +37,17 @@ public class ParamParseUtil {
         return -1;
     }
 
-    public static String lowercaseFirstChar(String s) {
-        if (s == null || s.isEmpty()) {
-            return s; // 如果输入字符串为空或null，返回原字符串
+    public static String uppercaseFirstChar(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
         }
-
-        char firstChar = s.charAt(0);
-        char lowerFirstChar = Character.toLowerCase(firstChar);
-
-        if (s.length() == 1) {
-            return String.valueOf(lowerFirstChar); // 如果字符串只有一个字符
-        }
-
-        return lowerFirstChar + s.substring(1); // 处理多字符的字符串
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+        return sb.toString();
     }
 
-    public static <T> void handler(Class<T> clz, String trimLine, int curSpaceNum) throws Exception {
+
+    public static void handler(String trimLine, int curSpaceNum) throws Exception {
         //处理a:b 和 a:
         //topObj.a=new A()
         //如果有b set
@@ -60,11 +55,17 @@ public class ParamParseUtil {
         String[] splits = Arrays.stream(trimLine.split(":")).map(String::trim).toArray(String[]::new);
         Object topObj = objStack.peek();
 
-        Field field = topObj.getClass().getDeclaredField(lowercaseFirstChar(splits[0]));
+        Field field = Arrays.stream(topObj.getClass().getDeclaredFields())
+                .filter(f -> splits[0].equals(uppercaseFirstChar(f.getName())))
+                .findFirst().orElse(null);
+        if (field == null)
+            return;
+
         //不提供set方法，那么就直接set
         if (!Modifier.isPublic(field.getModifiers())) {
             field.setAccessible(true);
         }
+        //List ，自定义类型
         if (splits.length == 1) {
             // 检查字段是否是 List 类型或其子类
             if (List.class.isAssignableFrom(field.getType())) {
@@ -80,6 +81,17 @@ public class ParamParseUtil {
             preSpaceStack.push(curSpaceNum);
             field.set(topObj, objStack.peek());
         } else {
+            //含有value，那么直接需要set
+            //排除skip的情况
+            if (field.isAnnotationPresent(SkipMappingValueAnnotation.class)) {
+                return;
+            }
+            if (splits[1].equals("null") || splits[1].equals("NULL") || splits[1].equals("Null")) {
+                return;
+            }
+            //当发现对应的value是null或者字符串"NULL"，"null"、"Null"时，能正确处理原始类型默认值
+            //SkipMappingValueAnnotation标注在属性上，工具类则不处理该属性，不去覆盖对象默认值
+            //如果参数中的键值key是小写字符开头，请自动忽略，如参数中有cpu: 2，此时不应该将该值赋给对象中属性cpu，必须要求参数key都是大写的即Cpu: 2才进行适配。
             if (field.getType().equals(String.class)) {
                 field.set(topObj, splits[1]);
             } else if (field.getType().equals(BigDecimal.class)) {
@@ -107,6 +119,14 @@ public class ParamParseUtil {
 
     }
 
+    public static <T> T safeCast(Object obj, Class<T> type) {
+        if (type.isInstance(obj)) {
+            return type.cast(obj);
+        } else {
+            throw new ClassCastException("Cannot cast " + obj.getClass().getName() + " to " + type.getName());
+        }
+    }
+
     /**
      * "a:b"
      * "a:"
@@ -115,18 +135,18 @@ public class ParamParseUtil {
      * "-b"
      * 需要有层级信息，根据缩进信息得到
      * 第一步：构造对象；
-     *      1. 原始类型
-     *      2. String, BigDecimal
-     *      3. ArrayList
-     *      4. 自定义Obj
+     * 1. 原始类型
+     * 2. String, BigDecimal
+     * 3. ArrayList
+     * 4. 自定义Obj
      * 第二步：set
-     *      需要对skip注解跳过
-     *      1. value是空的, null, "NULL", "null", "Null"用默认值
+     * 需要对skip注解跳过
+     * 1. value是空的, null, "NULL", "null", "Null"用默认值
      * 第三步：进入高级对象
-     *      如何进入高级对象，需要有对象堆栈
-     *      如果高级对象是：
-     *      1. ArrayList
-     *      2. 自定义Obj
+     * 如何进入高级对象，需要有对象堆栈
+     * 如果高级对象是：
+     * 1. ArrayList
+     * 2. 自定义Obj
      */
     public static <T> T parse(Class<T> clz, String filePath) throws Exception {
         //read file
@@ -134,7 +154,7 @@ public class ParamParseUtil {
             objStack.push(clz.newInstance());
             preSpaceStack.push(-1);
 
-            String line;
+            String line = null;
             while ((line = bis.readLine()) != null) {
                 //跳过空行
                 if (line.trim().isEmpty()) continue;
@@ -160,27 +180,51 @@ public class ParamParseUtil {
                     Method addMethod = topObj.getClass().getDeclaredMethod("add", Object.class);
                     //get parameterized type
                     //-b
-                    if (line.indexOf(":") == -1) {
+                    if (!line.contains(":")) {
                         //不是自定义的类型，那么是原始类型，String, BigDecimal
+                        //含有value需要直接set,且这里不会有skip注解
                         //get value
                         String value = line.substring(getFirstIndexIgnoreFirstChar(line, '-')).trim();
-                        if (String.class.isAssignableFrom(innerClazzStack.peek())) {
-                            addMethod.invoke(topObj, value);
+                        //当发现对应的value是null或者字符串"NULL"，"null"、"Null"时，能正确处理原始类型默认值
+                        if(!value.equals("null") && !value.equals("NULL") && !value.equals("Null")){
+                            if (String.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, value);
+                            }else if(BigDecimal.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, new BigDecimal(value));
+                            }else if(int.class.isAssignableFrom(innerClazzStack.peek()) || Integer.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Integer.parseInt(value));
+                            }else if(boolean.class.isAssignableFrom(innerClazzStack.peek()) || Boolean.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Boolean.parseBoolean(value));
+                            }else if(long.class.isAssignableFrom(innerClazzStack.peek()) || Long.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Long.parseLong(value));
+                            }else if(double.class.isAssignableFrom(innerClazzStack.peek()) || Double.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Double.parseDouble(value));
+                            }else if(float.class.isAssignableFrom(innerClazzStack.peek()) || Float.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Float.parseFloat(value));
+                            }else if(short.class.isAssignableFrom(innerClazzStack.peek()) || Short.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Short.parseShort(value));
+                            }else if(byte.class.isAssignableFrom(innerClazzStack.peek()) || Byte.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, Byte.parseByte(value));
+                            }else if(char.class.isAssignableFrom(innerClazzStack.peek()) || Character.class.isAssignableFrom(innerClazzStack.peek())) {
+                                addMethod.invoke(topObj, value.charAt(0));
+                            }else {
+                                throw new RuntimeException("不支持的类型");
+                            }
                         }
                     } else {
                         addMethod.invoke(topObj, innerClazzStack.peek().newInstance());
                         //入栈，处理-后面的
                         //get last of topObj and push to stack
                         preSpaceStack.push(curSpaceNum);
-                        objStack.push(((List) topObj).get(((List) topObj).size() - 1));
-                        handler(clz, line.substring(line.indexOf("-") + 1).trim(), getFirstIndexIgnoreFirstChar(line, '-'));
+                        objStack.push(((List<?>) topObj).get(((List<?>) topObj).size() - 1));
+                        handler(line.substring(line.indexOf("-") + 1).trim(), getFirstIndexIgnoreFirstChar(line, '-'));
                     }
                 } else {
-                    handler(clz, line.trim(), curSpaceNum);
+                    handler(line.trim(), curSpaceNum);
                 }
             }
             //return the bottom of objStack
-            return (T) objStack.firstElement();
+            return safeCast(objStack.firstElement(), clz);
         }
     }
 }
